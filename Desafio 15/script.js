f5dd5746fad6ad2eec776e0f15df0ee32b3f15e8
Lucelho <lucelhocristiano28@gmail.com>
2024-07-04 15:12:00 -0300

    Update script.js

--- Desafio 15/script.js
+++ Desafio 15/script.js
@@ -1,4 +1,7 @@
-let totalMinutos = 125;
-let horas = Math.floor(totalMinutos / 60);
-let minutos = totalMinutos % 60;
-console.log("O total de minutos é " + totalMinutos);
+let totalMinutos
+
+let horas = Math.floor(totalMinutos / 60);  
+let minutos = totalMinutos % 60;          
+
+console.log("Total de horas:", horas);
+console.log("Total de minutos:", minutos);
